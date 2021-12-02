b89958d7aa806ed9ea87465702822507584c237d
Huy <dodaihoc318@gmail.com>
2021-12-02 09:27:57 +0700

    completed screen

---
+++ lib/screen/completed.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CompletedScreen extends StatefulWidget {
+  const CompletedScreen({Key? key}) : super(key: key);
+
+  @override
+  _CompletedScreenState createState() => _CompletedScreenState();
+}
+
+class _CompletedScreenState extends State<CompletedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Completed'),
+      ),
+    );
+  }
+}
+
