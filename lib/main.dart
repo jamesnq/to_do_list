5809edcfbf1fc2a7f18098c0e10dc3accb9efdbd
Huy <dodaihoc318@gmail.com>
2021-12-02 11:26:02 +0700

    change theme color

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_list/screen/mainscreen.dart';
+import 'package:flutter/src/material/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,15 +8,16 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'To Do List',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
+        scaffoldBackgroundColor: Color(0xFFF5F5F5),
       ),
       home: const MainScreen(),
     );
